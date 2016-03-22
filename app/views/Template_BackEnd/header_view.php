755ac33dceb65ecdca62a619175f88b8b0cee3aa
savashan0019 <savasciftci1905@gmail.com>
2016-03-22 13:15:30 +0200

    Projenin tamamı bitmiştir en ufak sorun yoktur hayırlı olsun 22/03/2016  02:37 AM

--- app/views/Template_BackEnd/header_view.php
+++ app/views/Template_BackEnd/header_view.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>AdminPaneli | Akzer Mobilya</title>
+        <title>AdminPaneli | Yedi Tepe Teknik Servis</title>
         <!-- Tell the browser to be responsive to screen width -->
         <link rel="shortcut icon" href="<?php echo SITE_FRONT_ASSETS_DISTIMG; ?>/favicon.ico">
         <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
